8ce137235e890a27673779fb43f83bdf4725bc35
Smotko <ensmotko@gmail.com>
2011-06-28 01:02:54 +0200

    Removed +1

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -19,8 +19,6 @@ $(document).ready(function(){
     };
     setTimeout(moveHeader, Math.random()*10000+2000);
     
-    $('.navigation').append('<li class="paddless"><g:plusone size="small" count="false"></g:plusone></li>')
-    
     $('.chat:nth-child(odd)').addClass('emphasize');
     
 });
