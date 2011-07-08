54558d93d46b3c6fc0184fcd9df310a38281b168
Smotko <ensmotko@gmail.com>
2011-07-08 13:02:13 +0200

    Added animations to long chat boxes

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -4,22 +4,26 @@ function hideLongChat(){
     
     $('.comment_content').each(function(){
         if($(this).height() > MAX_HEIGHT){
+           $(this).data('originalHeight', $(this).height());
+           
            $(this).height(SET_HEIGHT);
            $(this).siblings('.comment_dots').show()           
         }
     });
     
     $('.chat').hover(function(){
-    if($('.comment_content', this).height() < SET_HEIGHT)
-        return;
-    $('.comment_content', this).height('auto');
-    $('.comment_dots', this).hide();
-
+        if($('.comment_content', this).height() < SET_HEIGHT)
+            return;
+        
+        $('.comment_dots', this).animate( {opacity: 0}, {duration: 200, easing: 'swing', queue: false});
+        $('.comment_content', this).animate( {height: $('.comment_content', this).data('originalHeight')}, {duration: 200, easing: 'swing', queue: false});
+        $('.comment_dots', this).hide();
     }, function(){
         if($('.comment_content', this).height() > MAX_HEIGHT)
         {
-            $('.comment_content', this).height(SET_HEIGHT);
             $('.comment_dots', this).show();
+            $('.comment_content', this).animate( {height: SET_HEIGHT}, {duration: 200, easing: 'swing', queue: false});
+            $('.comment_dots', this).animate( {opacity: 1}, {duration: 200, easing: 'swing', queue: false});
         }
     });
 }
