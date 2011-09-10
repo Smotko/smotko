ff8375dc68067d6c3b53aca12e69681bb9a6de00
Smotko <ensmotko@gmail.com>
2011-09-10 18:13:46 +0200

    Disabled the move header plugin

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -4,21 +4,21 @@ $(document).ready(function(){
         $(this).parent().parent().hide();
     });
     
-    var bg = Math.random()*10000;
-    $('.header_bg').css({backgroundPosition: bg + 'px 0px'});
-    moveHeader = function(){
-    	var newbg;
-    	if (Math.random() < 0.5){
-    		newbg = bg - Math.floor(Math.random()*100) - 50;
-    	}
-    	else{
-    		newbg = bg + Math.floor(Math.random()*100) + 50;
-    	}
-    	$('.header_bg').css({backgroundPosition: bg + 'px 0px'}).animate({backgroundPositionX: newbg+'px'}, 'slow', 'swing');
-    	bg = newbg;
-    	setTimeout(moveHeader, Math.random()*10000+2000)
-    };
-    setTimeout(moveHeader, Math.random()*10000+2000);
+//    var bg = Math.random()*10000;
+//    $('.header_bg').css({backgroundPosition: bg + 'px 0px'});
+//    moveHeader = function(){
+//    	var newbg;
+//    	if (Math.random() < 0.5){
+//    		newbg = bg - Math.floor(Math.random()*100) - 50;
+//    	}
+//    	else{
+//    		newbg = bg + Math.floor(Math.random()*100) + 50;
+//    	}
+//    	$('.header_bg').css({backgroundPosition: bg + 'px 0px'}).animate({backgroundPositionX: newbg+'px'}, 'slow', 'swing');
+//    	bg = newbg;
+//    	setTimeout(moveHeader, Math.random()*10000+2000)
+//    };
+//    setTimeout(moveHeader, Math.random()*10000+2000);
 
     $('.navigation').append('<li class="paddless"><g:plusone size="small" count="false"></g:plusone></li>')
     
